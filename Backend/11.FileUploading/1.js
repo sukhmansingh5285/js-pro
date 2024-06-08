92dfd665fc42b5de5cff7df791a35bc94297cb82
Sukhman Singh <sukhmansingh5285@gmail.com>
2024-06-08 22:08:11 +0530

    start from the 11

---
+++ Backend/11.FileUploading/1.js
@@ -0,0 +1,11 @@
+/* most of the file handling isn't done on own server, their are specialized third party services for it or AWS
+   Cloudinary is one of those third party services, 
+
+   npm install cloudinary
+
+*/
+
+// to store file in backend we need two packages -> multer, express-fileupload
+
+/* Cloudinary
+ */
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Description
 
-Advance JS
+Advance JS and BackEnd

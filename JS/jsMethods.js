5fe1116a2f6296b52c8e1019888072d98c94983b
Sukhman Singh <sukhmansingh5285@gmail.com>
2024-07-21 18:36:33 +0530

    jsMethods.js to be continued

---
+++ JS/jsMethods.js
@@ -0,0 +1,5 @@
+//map 
+const arr1 = [1,2,3,4,5]
+const mapArr = arr1.map((num)=> num*2)
+console.log(mapArr); // [ 2, 4, 6, 8, 10 ], returns an array with the operation on all the numbers
+
---
+++ React/Theory/Top100React.jsx
@@ -0,0 +1,3 @@
+// starts from 1:21
+// what are react components? what are the main elements of it?
+
